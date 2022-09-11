eb50e3d01e50ba824fbe140c1da1cff967e04dd6
Kamil Piróg <62013599+kpirog@users.noreply.github.com>
2022-09-11 17:47:43 +0200

    Import dotween package, Create door animation class

--- Assets/_PROJECT/Scripts/DoorSystem/BaseDoor.cs
+++ Assets/_PROJECT/Scripts/DoorSystem/BaseDoor.cs
@@ -4,6 +4,13 @@ namespace Woodland.DoorSystem
 {
     public abstract class BaseDoor : MonoBehaviour
     {
+        private DoorAnimation doorAnimation;
+
+        protected virtual void Awake()
+        {
+            doorAnimation = GetComponent<DoorAnimation>();
+        }
+
         protected abstract void OnTriggerEnter(Collider other);
         protected abstract void OnTriggerExit(Collider other);
     }
---
+++ Assets/_PROJECT/Scripts/DoorSystem/DoorAnimation.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using DG.Tweening;
+
+namespace Woodland.DoorSystem
+{
+    public class DoorAnimation : MonoBehaviour
+    {
+        public void OpenDoor()
+        {
+
+        }
+    }
+}
