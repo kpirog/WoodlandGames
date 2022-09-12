e79dbde798186b508cc6cd4450c90d08611dab6b
Kamil Piróg <62013599+kpirog@users.noreply.github.com>
2022-09-12 09:30:01 +0200

    Base main menu appearance

---
+++ Assets/_PROJECT/Scripts/UI/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Woodland.UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void PlayButton()
+        {
+
+        }
+
+        public void QuitButton()
+        {
+            Application.Quit();
+        }
+    }
+}
